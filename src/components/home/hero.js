f571840819e74d3f4a59a748309ebe2630dda5b2
Patrick San Juan <psanjuan06.sj@gmail.com>
2021-12-29 01:43:12 -0800

    update recently worked with list

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -16,9 +16,20 @@ const HeroHome = () => (
       <div className={heroStyles.aboutWrapper}>
         <p className="code">Hi, my name is</p>
         <h1>Patrick San Juan</h1>
-        <p>I'm a developer who likes to learn new technologies through projects.
-          I primarily do back end development, but I'm learning front-end web develop when I can to expand my skills.
-          Currently, I'm focused on developing software at FORM.</p>
+        <p>I like learning new technologies through projects.
+          I'm <b>primarily a back end developer</b>, but I'm learning front-end web technologies to <b>expand my skills</b>.
+          Currently, I'm focused on developing software at <b>FORM</b>.</p>
+        <p>
+          Lately, I've been working the with following technologies:
+          <ul className={heroStyles.recentSkills}>
+            <li>NodeJs</li>
+            <li>JavaScript/TypeScript</li>
+            <li>SQL</li>
+            <li>AWS</li>
+            <li>React</li>
+            <li>Vue</li>
+          </ul>
+        </p>
       </div>
     </div>
   </section>
--- src/components/resume/skills.js
+++ src/components/resume/skills.js
@@ -4,7 +4,7 @@ import * as heroStyles from "../../styles/home/hero.module.scss"
 
 const Skills = () => (
   <section className={heroStyles.wrapper}>
-    <h1>Skills</h1>
+    <h1>Skills, Technologies and Languages</h1>
     <p>This will be the skills section.</p>
   </section>
 )
