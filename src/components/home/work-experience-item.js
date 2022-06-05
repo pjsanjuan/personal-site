c6759c717098d68430b4d83e31504d5554bf5b3a
Patrick San Juan <psanjuan06.sj@gmail.com>
2022-06-04 23:56:02 -0700

    complete work exeperience initial implementation
    - TODO: needs refactor for code quality

--- src/components/home/work-experience-item.js
+++ src/components/home/work-experience-item.js
@@ -1,44 +1,79 @@
 import React from "react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-// MUI imports
-import Accordion from "@mui/material/Accordion"
-import AccordionSummary from "@mui/material/AccordionSummary"
-import AccordionDetails from "@mui/material/AccordionDetails"
 import Typography from "@mui/material/Typography"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import { Stack, Divider } from "@mui/material"
+import IconButton from '@mui/material/IconButton'
+import {
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component"
+import WorkIcon from "@mui/icons-material/Work"
+import Collapse from "@mui/material/Collapse"
+import { styled } from '@mui/material/styles'
+import Stack from "@mui/material/Stack"
+import "../../styles/work-experience-item.scss"
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const WorkExperienceItem = ({
+  id,
   position,
   company,
-  location,
   startDate,
   endDate,
   mdxBody,
-}) => (
-  <Accordion>
-    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        divider={<Divider orientation="vertical" flexItem />}
-        spacing={1}
-        width="100%"
-      >
+}) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <VerticalTimelineElement
+      key={id}
+      className="vertical-timeline-element--work"
+      contentStyle={{ background: "#8ea8c3", color: "#161925" }}
+      contentArrowStyle={{ borderRight: "7px solid  #8ea8c3" }}
+      date={`${startDate} to ${endDate}`}
+      iconStyle={{ background: "#8ea8c3", color: "#161925" }}
+      icon={<WorkIcon />}
+      style={{ width: "100%" }}
+      dateClassName="date-style"
+    >
+      <Typography variant="h6">
+        {position}
+      </Typography>
+      <Typography variant="h6">
+        <Stack direction="row" justifyContent="space-between">
+          <i>{company}</i>
+
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="see details"
+          >
+            <ExpandMoreIcon sx={{ color: "#fff" }} />
+          </ExpandMore>
+        </Stack>
+      </Typography>
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <Typography>
-          {position} at {company}
+          {<MDXRenderer>{mdxBody}</MDXRenderer>}
         </Typography>
-        <Typography>{location}</Typography>
-        <Typography>
-          <span>{startDate}</span> -{" "}
-          <span>{endDate ? endDate : "Present"}</span>
-        </Typography>
-      </Stack>
-    </AccordionSummary>
-    <AccordionDetails>
-      <Typography>{<MDXRenderer>{mdxBody}</MDXRenderer>}</Typography>
-    </AccordionDetails>
-  </Accordion>
-)
+      </Collapse>
+    </VerticalTimelineElement>
+  )
+}
 
 export default WorkExperienceItem
--- src/components/home/work-experience.js
+++ src/components/home/work-experience.js
@@ -2,15 +2,10 @@ import React from "react"
 
 import { useStaticQuery, graphql } from "gatsby"
 import WorkExperienceItem from "./work-experience-item"
-import Stack from "@mui/material/Stack"
 import { Typography } from "@mui/material"
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component"
-import WorkIcon from "@mui/icons-material/Work"
+import { VerticalTimeline } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css"
-import { MDXRenderer } from "gatsby-plugin-mdx"
+
 
 const workExperienceQuery = graphql`
   query {
@@ -40,35 +35,22 @@ const WorkExperience = ({ className }) => {
 
   return (
     <section className={className} id="work-experience">
-      {/* TODO: Wrap the VerticalTimeline component into it's own */}
       <Typography gutterBottom variant="h4">
         01. Work Experience
       </Typography>
 
-      <VerticalTimeline>
-        {allNodes.map(activeNode => (
-          <VerticalTimelineElement
-            key={activeNode.id}
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-            // date="2011 - present"
-            date={`${activeNode.frontmatter.startDate} to ${activeNode.frontmatter.endDate}`}
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<WorkIcon />}
-            style={{ width: "100%" }}
-          >
-            <Typography variant="h6">
-              {activeNode.frontmatter.position}
-            </Typography>
-            <Typography variant="h6">
-              <i>{activeNode.frontmatter.company}</i>
-            </Typography>
-
-            <Typography>
-              {<MDXRenderer>{activeNode.body}</MDXRenderer>}
-            </Typography>
-          </VerticalTimelineElement>
+      <VerticalTimeline
+        lineColor="#161925"
+      >
+        {allNodes.map(node => (
+          <WorkExperienceItem
+            key={node.id}
+            position={node.frontmatter.position}
+            company={node.frontmatter.company}
+            startDate={node.frontmatter.startDate}
+            endDate={node.frontmatter.endDate}
+            mdxBody={node.body}
+          />
         ))}
       </VerticalTimeline>
     </section>
